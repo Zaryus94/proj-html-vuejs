160933d13d9a63b094240c5a29b20793c86f14ec
Christian Podda <poddachristian94@gmail.com>
2022-04-26 18:56:59 +0200

    Init

---
+++ src/components/HeaderComp.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="header">
+    <h4>Avada & Associates legal team announce deal to open up oil & gas</h4>
+  </div>
+</template>
+
+<script>
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+</style>
