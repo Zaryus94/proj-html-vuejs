96e6ec14e88f29ea4c5512a4aed9678acd656908
Christian Podda <poddachristian94@gmail.com>
2022-04-26 20:57:10 +0200

    General fix & Header update

--- src/App.vue
+++ src/App.vue
@@ -22,12 +22,9 @@ export default {
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  box-sizing: border-box;
+  margin: 0;
+  font-family: sans-serif;
 }
 </style>
--- src/components/HeaderComp.vue
+++ src/components/HeaderComp.vue
@@ -1,12 +1,47 @@
 <template>
   <div class="header">
-    <h4>Avada & Associates legal team announce deal to open up oil & gas</h4>
+    <hr />
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          <h4>
+            Avada & Associates legal team announce deal to open up oil & gas
+          </h4>
+          <span class="SocialIcon">
+            <i class="fa-brands fa-facebook-square"></i>
+          </span>
+          <span class="SocialIcon">
+            <i class="fa-brands fa-twitter-square"></i>
+          </span>
+          <span class="SocialIcon">
+            <i class="fa-brands fa-instagram-square"></i>
+          </span>
+          <span class="SocialIcon">
+            <i class="fa-brands fa-youtube-square"></i>
+          </span>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
+
 
 <script>
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
+
 <style scoped lang="scss">
+hr {
+  border: 3px solid #54a096;
+}
+h4 {
+  font-weight: normal;
+  margin: 1% 20%;
+  display: inline-block;
+}
+.SocialIcon {
+  padding: 10px;
+  font-size: 30px;
+  color: #54a096;
+}
 </style>
